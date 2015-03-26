3dfdd20c6afb5560be8b017812cccb7fff073f22
Murphy Berzish <murphy.berzish@gmail.com>
2015-03-26 10:38:14 -0400

    generation of legendre polynomials

--- integrator.py
+++ integrator.py
@@ -6,6 +6,26 @@ import nengo
 from nengo.utils.functions import piecewise
 from nengo.utils.ensemble import tuning_curves
 import scipy.interpolate
+import scipy.special
+
+# Legendre polynomial of degree n.
+# The closed-form representation is
+# P_n(x) = 2^n * (sum from k=0 to n) x^k * (n choose k)*([n+k+-1]/2 choose n) 
+def legendre(n):
+    coeffs = []
+    for k in range(n+1):
+        c1 = scipy.special.binom(n, k)
+        c2 = scipy.special.binom((n+k-1)/2, n)
+        coeffs.append(c1 * c2)
+    def L(x):
+        accumulator = np.zeros(x.shape)
+        argument = np.ones(x.shape)
+        for k in range(n+1):
+            accumulator += np.multiply(argument, coeffs[k])
+            argument = np.multiply(argument, x)
+        return 2.0**n * accumulator
+    return L
+
 
 model = nengo.Network(label='Integrator')
 with model:
